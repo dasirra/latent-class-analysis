c9f89528b7ca91048b2b913c5870581369bede1e
Daniel <dasirra@gmail.com>
2016-06-04 11:53:29 +0200

    Fix minor problems

--- src/gmm.py
+++ src/gmm.py
@@ -13,14 +13,16 @@ class GMM():
         # flag to indicate if converged
         self.converged_ = False
 
+        # verbose level
+        self.verbose = 0
+
     def _do_e_step(self, X):
 
         n_rows, n_cols = np.shape(X)
         r_numerator = np.zeros(shape=(n_rows,self.n_components))
         for k in range(self.n_components):
-            r_numerator[:,k] =
-            	self.weigths_[k]*sc.stats.multivariate_normal.pdf(X,
-            		mean=self.means_[k],cov=self.covars_[k])
+            r_numerator[:,k] = self.weigths_[k]*sc.stats.multivariate_normal.pdf(
+                X,mean=self.means_[k],cov=self.covars_[k])
         r_denominator = np.sum(r_numerator,axis=1)
         r = r_numerator/np.tile(r_denominator,(self.n_components,1)).T
         return r
@@ -47,8 +49,8 @@ class GMM():
             cov_numerator = np.zeros((n_rows,n_cols,n_cols))
             for n in range(n_rows):
                 aux = X[n,:]-self.means_[k]
-                cov_numerator[n,:,:] = responsibility[n,k]*
-                    np.dot(aux[:,None],aux[:,None].T)
+                cov_numerator[n,:,:] = responsibility[n,k]*np.dot(
+                    aux[:,None],aux[:,None].T)
             cov_numerator = np.sum(cov_numerator,axis=0)
             cov_denominator = np.sum(responsibility[:,k])
             self.covars_[k] = cov_numerator/cov_denominator
@@ -68,7 +70,7 @@ class GMM():
         if self.verbose > 0:
             print('EM algorithm started')
 
-        self.weigths_ = sc.stats.dirichlet.rvs(np.ones(shape=n_components))[0]
+        self.weigths_ = sc.stats.dirichlet.rvs(np.ones(shape=self.n_components))[0]
 
         self.means_ = sc.stats.multivariate_normal.rvs(
                     mean=np.mean(X,axis=0),
@@ -76,31 +78,31 @@ class GMM():
                     size=self.n_components)
 
         covars = np.zeros((self.n_components,n_cols,n_cols))
-        for k in self.n_components:
+        for k in range(self.n_components):
             covars[k,:,:] = sc.stats.uniform.rvs()*np.eye(N=n_cols,M=n_cols)
         self.covars_ = covars
 
-        self.ll_ = []
+        self.ll_ = [-np.inf]
 
         for i in range(self.n_iter):
             if self.verbose > 0:
                 print('\tEM iteration {n_iter}'.format(i))
 
             # E-step
-            responsibilities = _do_e_step(X)
+            responsibilities = self._do_e_step(X)
 
             # M-step
-            _do_m_step(X, responsibilities)
+            self._do_m_step(X, responsibilities)
 
             # Check for convergence
-            aux = np.zeros(shape=(N,K))
-	        for k in range(K):
-	            normal_prob = sc.stats.multivariate_normal.pdf(X,
-	            	mean=self.means_[k],cov=self.covars_[k])
-	            aux[:,k] = self.weigths_[k]*normal_prob
-	        ll_val = np.sum(np.log(np.sum(aux,axis=1)))
-	        if np.abs(ll_val-ll[-1])<self.tol:
-	            break
-	        else:
-	            self.ll_.append(ll_val)
-	            
+            aux = np.zeros(shape=(n_rows,self.n_components))
+            for k in range(self.n_components):
+                normal_prob = sc.stats.multivariate_normal.pdf(X,
+                    mean=self.means_[k],cov=self.covars_[k])
+                aux[:,k] = self.weigths_[k]*normal_prob
+            ll_val = np.sum(np.log(np.sum(aux,axis=1)))
+            if np.abs(ll_val-self.ll_[-1])<self.tol:
+                break
+            else:
+                self.ll_.append(ll_val)
+                
